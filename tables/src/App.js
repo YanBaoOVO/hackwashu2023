df2371201e7127217b7d2fd9d37509b0d343f540
ryan <yongyan@wustl.edu>
2023-10-14 14:01:24 -0500

    Login

--- tables/src/App.js
+++ tables/src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Login from "./user/login";
 
 function App() {
+    const [isLoginModalOpen, setLoginModalOpen] = useState(false);
     return (
         <div className="App">
             <header className="App-header">
-                <div className="Login">
-                    <button>Login</button>
+                <div className="left-space"></div>
+
+                <div className="login">
+                    <div className = "login-button">
+                        <button onClick={() => setLoginModalOpen(true)}>Login</button>
+                        {isLoginModalOpen && (
+                            <div className="login-modal">
+                                <div className="login-box">
+                                    <button onClick={() => setLoginModalOpen(false)}>Close</button>
+                                    <Login />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+
                 </div>
+
                 <div className = "title">
                     <h1>Tables</h1>
                 </div>
+
                 <div className="search">
                     <input type="text" placeholder="Search" />
                     <button>Search</button>
                 </div>
+
+                <div className="right-space"></div>
             </header>
 
             <nav>
