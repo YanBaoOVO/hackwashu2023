77e7afb3889b8d6370697dc7940a13653c108ca6
Carl <carl.w@wustl.edu>
2023-10-14 12:19:14 -0500

    firebase queries

--- tables/src/App.js
+++ tables/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs, setDoc, doc } from 'firebase/firestore'; // Import setDoc and doc
+import { db } from './firebase';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+
+    const addUser = async () => {
+        try {
+            // Use setDoc to add data to the "cities" collection
+            await setDoc(doc(db, "users"), {
+                username: "User 1",
+                password: "password",
+            });
+            console.log("Document successfully written!");
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    };
+
+    const fetchUsers = async () => {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        const newData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        setUsers(newData);
+    }
+
+    useEffect(() => {
+        fetchUsers();
+        addUser(); // Call the function to add a city when the component mounts
+    }, []);
+
+    return (
+        <div>
+            <h1>User List</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        <strong>ID:</strong> {user.id}<br />
+                        <strong>Username:</strong> {user.username}<br />
+                        <strong>Password:</strong> {user.password}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
