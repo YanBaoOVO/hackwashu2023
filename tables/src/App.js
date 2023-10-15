18fb9b70be36f16eadff6b975b92dd9340383a6c
ryan <yongyan@wustl.edu>
2023-10-15 10:57:52 -0500

    Added register function

--- tables/src/App.js
+++ tables/src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
 import Login from "./login.js";
+import Register from "./register.js";
 
 function App() {
     const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+    const [isRegisterModalOpen, setRegisterModalOpen] = useState(false);
     return (
         <div className="App">
             <div className="account-box">
                 <button onClick={() => setLoginModalOpen(true)}>Login</button>
-                <div className='search-box'><input type="text" placeholder="Search"></input><button onClick={() => setLoginModalOpen(true)}>Search</button></div>
+                <button onClick={() => setRegisterModalOpen(true)}>Register</button>
+                <div className='search-box'>
+                    <input type="text" placeholder="Search"></input>
+                    <button >Search</button>
+                </div>
             </div>
             <h1 className='header-bar'>Tables</h1>
 
@@ -17,6 +23,15 @@ function App() {
                     <div className="login-box">
                         <button className="close-button" onClick={() => setLoginModalOpen(false)}>Close</button>
                         <Login />
+                    </div>
+                </div>
+            )}
+
+            {isRegisterModalOpen && (
+                <div className="register-modal">
+                    <div className="register-box">
+                        <button className="close-button" onClick={() => setRegisterModalOpen(false)}>Close</button>
+                        <Register />
                     </div>
                 </div>
             )}
